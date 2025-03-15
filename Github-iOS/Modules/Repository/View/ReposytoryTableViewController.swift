ab58b43e4c5d4a5b5a4e5d71b4b786f81a06589c
gabriel <gabrielcruzerence9993@gmail.com>
2025-03-15 15:34:00 -0300

    criado estrutura incial

---
+++ Github-iOS/Modules/Repository/View/ReposytoryTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReposytoryTableViewController.swift
+//  Github-iOS
+//
+//  Created by Gabriel on 14/03/25.
+//
+
+import UIKit
+
+class ReposytoryTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
